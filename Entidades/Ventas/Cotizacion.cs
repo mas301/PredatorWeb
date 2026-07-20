5e48f84d90d586404b241bc0ae4b8238077d672e
Manuel Aliaga S <mas301@hotmail.com>
2026-07-20 09:05:50 -0500

    coti

---
+++ Entidades/Ventas/Cotizacion.cs
@@ -0,0 +1,16 @@
+﻿namespace PredatorWeb.Entidades.Ventas
+{
+    public class Cotizacion : Entidad
+    {
+        public Cotizacion()
+        {
+            NombreEntidad = "Cotizacion";
+            NombreTabla ="VenCotizacion";
+            NombreProcedimiento="VenCotizacionMantenimimento";
+            NombreVista= "VenCotizacionVistaLista";
+            NombreClavePrimaria= "CotizacionId";
+            NombrePlural = "Cotizaciones";
+            Tipo = TipoEntidad.Maestro;
+        }
+    }
+}
